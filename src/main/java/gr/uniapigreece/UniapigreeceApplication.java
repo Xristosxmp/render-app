2256838e7ae43ad9b5cf196b7d080e4706634820
xristos <cotthebot.uop@gmail.com>
2024-03-10 22:59:50 +0200

    fix

---
+++ src/main/java/gr/uniapigreece/UniapigreeceApplication.java
@@ -0,0 +1,13 @@
+package gr.uniapigreece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniapigreeceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniapigreeceApplication.class, args);
+	}
+
+}
