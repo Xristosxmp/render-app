6650ddc7b17a492c37822aeaeafdc92f341be329
xristos <cotthebot.uop@gmail.com>
2024-03-10 23:18:24 +0200

    C

---
+++ src/test/java/gr/uniapigreece/UniapigreeceApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.uniapigreece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniapigreeceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
